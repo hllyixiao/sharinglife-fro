8a8994566cacc9dc92971d8a31af00e65aa9f981
RakehellBoy <1272474387@qq.com>
2018-02-24 17:16:17 +0800

    添加 路由(Router)

--- sharinglife/src/app/app.module.ts
+++ sharinglife/src/app/app.module.ts
@@ -8,10 +8,22 @@ import { CoreModule } from './core/core.module';
 // component
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', componnet: RegisterComponent },
+  // 当URL为空时就会访问那里
+  { path：'', redirectTo: '/home', pathMatch: 'full'},
+  // 当所请求的URL不匹配前面定义的路由表中的任何路径时，路由器就会选择此路由
+  { path: '**', component: PageNotFoundComponent}
+]
 
-@NgModule({
+@NgMod,
+    PageNotFoundComponentule({
   declarations: [
     AppComponent,
     HomePageComponent,
