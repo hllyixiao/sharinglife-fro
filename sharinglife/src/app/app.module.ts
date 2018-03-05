4e2edfbdd1c797b3608e63d8b5758bbbbedaf27b
RakehellBoy <1272474387@qq.com>
2018-03-05 17:41:53 +0800

    1.添加富文本框(wangEditor)  2.添加文章创建页面 (creation)

--- sharinglife/src/app/app.module.ts
+++ sharinglife/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { CreationComponent } from './creation/creation.component';
 
 
 
@@ -33,7 +34,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
     LoginComponent,
     RegisterComponent,
     PageNotFoundComponent,
-    FeedbackComponent
+    FeedbackComponent,
+    CreationComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
