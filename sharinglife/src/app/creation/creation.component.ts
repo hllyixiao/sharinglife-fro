0054d09c4674db181ead10a904778ffabbac2154
RakehellBoy <1272474387@qq.com>
2018-03-25 22:18:03 +0800

    引入 LocationStrategy  from '@angular/common'， 使用 pushState 无刷新变革url

--- sharinglife/src/app/creation/creation.component.ts
+++ sharinglife/src/app/creation/creation.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { LocationStrategy } from '@angular/common';
 import * as Editor from 'wangEditor';
 import { Subject } from 'rxjs/Subject';
+import * as _ from 'lodash';
 import 'rxjs/Rx';
 
 import { ArticleService } from '../core/article.service';
-import { EditorConfig } from '../_models/editor-config';
 import { UserService } from '../core/user.service';
 
+// class
 import { Article } from '../_models/article';
+import { EditorConfig } from '../_models/editor-config';
+import { environment as env} from '../../environments/environment';
 
 @Component({
   selector: 'app-creation',
@@ -22,15 +26,16 @@ export class CreationComponent implements OnInit {
     id: 0,
     state: 'draft',
     title: '',
-    content: '',
+    contentHtml: '',
     contentTxt: '',
-    length: 0,
+    contentSize: 0,
     userId: -1
   };
   public hideContentPlaceholder = false;
-  public hideWarningContent = true;
+  public showWarningContent = false;
   public warningContent = '';
   public editTitle = '';
+  public createArticle = true;
   public articleChangeSubject = new Subject();
 
   constructor(
@@ -39,14 +44,16 @@ export class CreationComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private eleRef: ElementRef,
-    private render: Renderer2) { }
+    private render: Renderer2,
+    private location: LocationStrategy) { }
 
   ngOnInit() {
     this.initWangEditor();
     this.hideHint();
-    //TODO this.article.userId = this.userService.user.id;
+    // TODO this.article.userId = this.userService.user.id;
     this.article.id = Number(this.route.snapshot.paramMap.get('articleId'));
     if (this.article.id > 0) { // 表示当前属于文章编辑状态，等于-1表示新创文章。
+      this.createArticle = false;
       this.articleService.getArticleById(this.article.id).subscribe(
         article => {
           if (article.content !== '<p><br></p>') {
@@ -56,6 +63,7 @@ export class CreationComponent implements OnInit {
           this.editTitle = article.title;
         }
       );
+
     }
     // 3s后没有事件触发就发送该请求
     this.articleChangeSubject.debounceTime(3000).subscribe(
@@ -71,31 +79,45 @@ export class CreationComponent implements OnInit {
     this.editor.customConfig.uploadFileName = 'file';
     this.editor.customConfig.withCredentials = true;
     this.editor.customConfig.uploadImgMaxSize = 5 * 1024 * 1024; // 图片大小
-    this.editor.customConfig.uploadImgMaxLength = 1; // 限制一次最多上传张图片
-    this.editor.customConfig.uploadImgTimeout = 10000; // 超时时长 默认10s
+    this.editor.customConfig.uploadImgMaxLength = 5; // 限制一次最多上传张图片
+    this.editor.customConfig.uploadImgTimeout = 100000; // 超时时长 默认10s
     this.editor.customConfig.uploadImgParams = {
-      'userId': 1, //TODO this.userService.user,
-      'articleId': 123012
+      'userId': 10009, // this.userService.user,
+      'articleId': 25
     };
 
     this.editor.customConfig.uploadImgHooks = {
+      before: function (xhr, editor, files) {
+        // 图片上传之前触发
+        if (thisComp.article.id === 0) {
+          thisComp.saveArticle();
+        }
+      },
       // 服务器端返回的不是 {errno:0, data: [...]} 这种格式，可使用该配置,但必须是一个 JSON 格式字符串
       customInsert: function (insertImg, result, editor) {
-        insertImg(result.url);
-        this.thisComp.article.id = result.article.id;
+        const imgList = result.data || [];
+        _.forEach(imgList, function(url){
+          insertImg(env.imgUrl + url);
+        });
       },
       success: function (xhr, editor, result) {
         // 图片上传并返回结果，图片插入成功之后触发
+          thisComp.createArticle = false;
       }
     };
-    // 上传图片的错误提示, 默认使用alert弹出
+    // 上传图片的错误提示
     this.editor.customConfig.customAlert = function (info) {
-      thisComp.hideWarningContent = false;
+      thisComp.showWarningContent = true;
       thisComp.warningContent = info;
     };
 
     this.editor.customConfig.onchange = function (html) {
-      thisComp.articleChangeSubject.next(thisComp.article);
+      if (thisComp.article.id === 0 && thisComp.createArticle) { // create 立即上传
+        thisComp.createArticle = false;
+        this.saveArticle();
+      }else { //  update延迟3s
+        thisComp.articleChangeSubject.next(thisComp.article);
+      }
     };
     this.editor.create();
   }
@@ -109,13 +131,17 @@ export class CreationComponent implements OnInit {
   }
 
   saveArticle() {
-    this.article.content = this.editor.txt.html();
-    this.article.contentTxt = this.getPureTxt(this.article.content);
-    this.article.length = this.getPureTxt(this.article.content).length;
+    this.article.contentHtml = this.editor.txt.html();
+    this.article.contentTxt = this.getPureTxt(this.article.contentHtml);
+    this.article.contentSize = this.getPureTxt(this.article.contentHtml).length;
 
     if (this.article.id === 0) { // 新建文章
       this.articleService.addArticle(this.article).subscribe(
-        req => this.router.navigate(['../edit/article', req.articleId])
+        articleId => {
+          this.article.id = articleId;
+          this.location.pushState('', '' , '../edit/article/' + articleId, '');
+        },
+        err => this.createArticle = true
       );
     }else { // 更新文章
       this.articleService.updateArticle(this.article).subscribe(
