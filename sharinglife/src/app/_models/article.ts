f9ee458b2e5bead8313584136fa542df05db6c0b
RakehellBoy <1272474387@qq.com>
2018-04-25 22:13:23 +0800

    文章发布接口对接

--- sharinglife/src/app/_models/article.ts
+++ sharinglife/src/app/_models/article.ts
@@ -1,7 +1,8 @@
 export class Article {
     id: number;
-    state: string; // 状态
+    status: number; // 状态 0 删除  1 草稿   2 发布
     title: string; // 文章title
+    allowcomments: number; // 是否允许评论 0 1
     contentHtml: string; // 文章内容 html格式
     contentTxt: string; // 文章内容 txt纯文本格式
     contentSize: number; // 文章长度
